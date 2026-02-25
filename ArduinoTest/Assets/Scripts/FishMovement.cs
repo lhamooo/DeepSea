58a55b38f56bcf62163af042cc0153f7ea52a72d
awille-zhdk <awille@zhdk.ch>
2026-02-25 10:38:42 +0100

    Add hunting behaviour to worm

--- ArduinoTest/Assets/Scripts/FishMovement.cs
+++ ArduinoTest/Assets/Scripts/FishMovement.cs
@@ -2,22 +2,25 @@ using UnityEngine;
 
 public class FishMovement : MonoBehaviour
 {
-    [SerializeField] private float moveSpeed = 0;
+    [SerializeField] private float moveSpeed = 1f;
     [SerializeField] private float maxDistance = 10f;
     [SerializeField] private float turnSpeedModifier = 1.5f;
     [SerializeField] private Vector3 currentTarget;
-    [SerializeField] private bool moveOnTrack = false;
-    [SerializeField] private float trackDeviation = 10f;
+
+    [Header("Hunting Behaviour")]
+    [SerializeField] private bool canHunt = false;
+    [SerializeField] private float huntSpeed = 2f;
+    [SerializeField] private float huntingTurnSpeedModifier = 2f;
+    [SerializeField] private GameObject huntingTarget;
+    private bool isHunting = false;
     private bool hasTarget = false;
-    Vector3 previousTarget;
+    private float currentMoveSpeed;
+    private float currentTurnSpeedModifier;
+
     void Start()
     {
-        if (moveOnTrack)
-        {
-            Vector3 max = new Vector3(maxDistance, maxDistance, maxDistance);
-            currentTarget = new Vector3(Random.Range(-max.x, max.x), Random.Range(-max.y, max.y), Random.Range(-max.z, max.z));
-            hasTarget = true;
-        }
+        currentMoveSpeed = moveSpeed;
+        currentTurnSpeedModifier = turnSpeedModifier;
     }
 
     void Update()
@@ -28,36 +31,87 @@ public class FishMovement : MonoBehaviour
             hasTarget = true;
         }
 
-        LookAtTarget();
-        transform.position += transform.forward * moveSpeed * Time.deltaTime;
-
-        if (Vector3.Distance(transform.position, currentTarget) < 0.1f)
+        if (canHunt && isHunting)
         {
-            previousTarget = currentTarget;
+            HuntingBehaviour();
+        }
+
+        LookAtTarget();
+        transform.position += transform.forward * currentMoveSpeed * Time.deltaTime;
+
+        if (Vector3.Distance(transform.position, currentTarget) < 0.1f && !isHunting)
+        {
             hasTarget = false;
         }
     }
 
     private Vector3 FindWaypoint()
     {
-        Vector3 waypoint = Vector3.zero;
         Vector3 max = new Vector3(maxDistance, maxDistance, maxDistance);
-        if (!moveOnTrack)
-        {
-            waypoint = new Vector3(Random.Range(-max.x, max.x), Random.Range(-max.y, max.y), Random.Range(-max.z, max.z));
-        }
-        else
-        {
-            waypoint = new Vector3(previousTarget.x + Random.Range(-trackDeviation, trackDeviation), previousTarget.y + Random.Range(-trackDeviation, trackDeviation), transform.forward.z * 10);
-        }
+        Vector3 waypoint = new Vector3(Random.Range(-max.x, max.x), Random.Range(-max.y, max.y), Random.Range(-max.z, max.z));
         return waypoint;
     }
 
     private void LookAtTarget()
     {
-        float turnSpeed = moveSpeed * turnSpeedModifier;
+        float turnSpeed = moveSpeed * currentTurnSpeedModifier;
 
         Vector3 lookDirection = currentTarget - transform.position;
         transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.LookRotation(lookDirection), turnSpeed * Time.deltaTime);
+    }
+
+    private void HuntingBehaviour()
+    {
+        if (huntingTarget == null)
+        {
+            huntingTarget = FindClosestBoid();
+            hasTarget = true;
+        }
+        else
+        {
+            currentTarget = huntingTarget.transform.position;
+            if (Vector3.Distance(transform.position, currentTarget) > 10f)
+            {
+                huntingTarget = FindClosestBoid();
+            }
+        }
+    }
+
+    private GameObject FindClosestBoid()
+    {
+        GameObject[] boids;
+        boids = GameObject.FindGameObjectsWithTag("Boid");
+        GameObject closestBoid = null;
+        float distance = Mathf.Infinity;
+        Vector3 pos = transform.position;
+        foreach (GameObject boid in boids)
+        {
+            if (Vector3.Distance(pos, boid.transform.position) < distance)
+            {
+                distance = Vector3.Distance(pos, boid.transform.position);
+                closestBoid = boid;
+            }
+        }
+
+        return closestBoid;
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("InteractionZone"))
+        {
+            isHunting = true;
+            currentMoveSpeed = huntSpeed;
+            currentTurnSpeedModifier = huntingTurnSpeedModifier;
+        }
+        else if (isHunting && other.gameObject == huntingTarget)
+        {
+            Destroy(other);
+            huntingTarget = null;
+            isHunting = false;
+            hasTarget = false;
+            currentMoveSpeed = moveSpeed;
+            currentTurnSpeedModifier = turnSpeedModifier;
+        }
     }
 }
---
+++ ArduinoTest/Assets/Scripts/InteractionRadius.cs
@@ -0,0 +1,40 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class InteractionRadius : MonoBehaviour
+{
+    private float expansionStrength;
+    private bool isExpanding = false;
+    private int timer;
+    void Update()
+    {
+        if (Keyboard.current.spaceKey.isPressed && timer == 0)
+        {
+            isExpanding = true;
+            expansionStrength++;
+        }
+        else if (isExpanding)
+        {
+            if (timer < expansionStrength)
+            {
+                timer++;
+                Vector3 s = transform.localScale;
+                Vector3 newScale = new Vector3(s.x + 0.001f * expansionStrength, s.y + 0.001f * expansionStrength, s.z + 0.001f * expansionStrength);
+                transform.localScale = newScale;
+            }
+            else
+            {
+                timer = 0;
+                expansionStrength = 0;
+                transform.localScale = new Vector3(0.1f, 0.1f, 0.1f);
+                isExpanding = false;
+            }
+
+        }
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        Debug.Log(other.gameObject.tag + " activated!");
+    }
+}
